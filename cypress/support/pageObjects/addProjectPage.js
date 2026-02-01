d8407356b8b68a71cdb8ab2d976c80da1d29d03d
Timilehin_1 <oyeniran.timilehin@gmail.com>
2026-02-01 23:58:50 +0100

    updated project setup tests

--- cypress/support/pageObjects/addProjectPage.js
+++ cypress/support/pageObjects/addProjectPage.js
@@ -2,6 +2,7 @@ const addProjectBtn = ".flex-row.gap-6 > .inline-flex";
 const projectNameField = '[name="name"]';
 const projectRefField = '[name="number"]';
 const saveProjectBtn = ".justify-end > .bg-primary";
+const successToast = ".toast-message";
 
 class AddProjectPage {
   clickAddProjectIcon() {
@@ -18,11 +19,35 @@ class AddProjectPage {
 
   clickSaveProjectBtn() {
     cy.get(saveProjectBtn).should("be.visible").click();
+    cy.get(successToast).should("be.visible");
   }
 
   verifyProjectCreated(projectName) {
     // Check if project with this name exists in UI
-    cy.contains(projectName).should("be.visible");
+    cy.contains(projectName).should("not.be.visible");
+  }
+  clickProjectOptions(projectName) {
+    cy.contains(projectName)
+      .parent()
+      .within(() => {
+        cy.get(".options-button").click();
+      });
+  }
+
+  clickEditProject() {
+    cy.get(".edit-project-option").click();
+  }
+
+  clickDeleteProject() {
+    cy.get(".delete-project-option").click();
+  }
+
+  confirmDeleteProject() {
+    cy.get(".confirm-delete-button").click();
+    cy.get(successToast).should("be.visible");
+  }
+  verifyProjectDeleted(updatedProjectName) {
+    cy.contains(updatedProjectName).should("not.exist");
   }
 }
 export const addProjectPage = new AddProjectPage();
