ef1d9a244fd7f1ddfc1f1ae68b10426c2a39e35b
Timilehin_1 <oyeniran.timilehin@gmail.com>
2026-02-02 09:17:44 +0100

    login command update

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,48 +1,3 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-
-/*Cypress.Commands.add("login", () => {
-  cy.visit("/");
-
-  cy.get('input[data-slot="input"]').type("qa@coreloops.ai");
-
-  // 2FA workaround assumption: session already trusted
-  cy.contains("Dashboard", { timeout: 20000 }).should("be.visible");
-}); */
-
-/*Cypress.Commands.add("login", () => {
-  cy.visit("/");
-  cy.get('input[data-slot="input"]').type("qa@coreloops.ai");
-  cy.get('button[data-slot="button"]').should("be.visible").click();
-  // Manual OTP entry here once
-  cy.wait(2000);
-  cy.get('button[data-slot="button"]').should("be.visible").click();
-  cy.get("#radix-_r_7_ > div > span").contains("Dashboard");
-});*/
-
 Cypress.Commands.add("createProject", (projectName) => {
   cy.get("button").contains("+").click();
   cy.get("input").first().type(projectName);
@@ -58,14 +13,6 @@ Cypress.Commands.add("login", () => {
     // Manual OTP ONCE
     cy.wait(40000);
     cy.get('button[data-slot="button"]').click();
-    cy.get("#radix-_r_7_").contains("Dashboard"); // stable assertion
+    cy.get("#radix-_r_7_").contains("Dashboard");
   });
-});
-Cypress.Commands.add("loginUI", () => {
-  cy.visit("/");
-  cy.get('input[data-slot="input"]').type(Cypress.env("email"));
-  cy.get('button[data-slot="button"]').click();
-  cy.wait(40000); // Manual OTP
-  cy.get('button[data-slot="button"]').click();
-  cy.contains("Dashboard");
 });
